6e1c678e67dfbe6ab2e91a3fc79b7151aa5395eb
Arjun Devendra <arjundevendra34@gmail.com>
2024-12-06 23:03:38 +0530

    path

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+const path = require("path");
 
 const connectDB = require("./dbConfig/connectDB");
 
@@ -9,13 +10,13 @@ const app = express();
 dotenv.config();
 app.use(cors({ origin: "https://quizathon-front-end.onrender.com" }));
 
-// // Serve static assets
-// app.use(express.static(path.join(__dirname, "build")));
+// Serve static assets
+app.use(express.static(path.join(__dirname, "build")));
 
-// // Fallback for all other routes (for React Router)
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+// Fallback for all other routes (for React Router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 connectDB();
 app.use(express.json());
